c711f8b9ad0edd29f5b29a89639c94a2d33fafb1
mysterlee418 <mysterlee418@gmail.com>
2023-01-30 14:59:51 +0900

    [mysterlee] title scene

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Copying mobile TangramGame
 
 2023-01-30 / v0.1.0 / Setup Tangram Game
+2023-01-30 / v0.2.0 / Setup Tangram Game TitleScene
+
---
+++ TangramGame/Assets/01.UnityProject/Scripts/Global/GData+Define.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static partial class GData {
+
+    public const string SCENE_NAME_PLAY = "02.PlayScene";
+
+
+}
